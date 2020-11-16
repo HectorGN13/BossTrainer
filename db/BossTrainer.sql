57dbe556f2e3cf15bd8a24ff95bfb3ec06076fbc
Hector E. Garcia Nuñez <hectoreduardo.garcia@iesdonana.org>
2020-11-16 19:50:59 +0100

    intentando desplegar heroku "intento 3"

---
+++ db/BossTrainer.sql
@@ -0,0 +1,12 @@
+-- Database: BossTrainer
+
+-- DROP DATABASE "BossTrainer";
+
+CREATE DATABASE "BossTrainer"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Spain.1252'
+    LC_CTYPE = 'Spanish_Spain.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
