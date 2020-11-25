174a09138c6af01b542c9bf05404ff17d69ea5b2
Hector E. Garcia Nuñez <hectoreduardo.garcia@iesdonana.org>
2020-11-25 16:56:03 +0100

    Añadido columna id_provincia tabla gym
    Añadido micracion que crea una columna de id_provincia clave ajena que apunta a la tabla provincia

---
+++ console/migrations/m201125_153346_add_id_provincia_column_to_gym_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding columns to table `{{%gym}}`.
+ */
+class m201125_153346_add_id_provincia_column_to_gym_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%gym}}', 'id_provincia', $this->integer()->defaultValue(null));
+
+        $this->createIndex(
+            '{{%idx-gym-id_provincia}}',
+            '{{%gym}}',
+            'id_provincia'
+        );
+
+        // add foreign key for table `{{%gym}}`
+        $this->addForeignKey(
+            '{{%fk-gym-id_provincia}}',
+            '{{%gym}}',
+            'id_provincia',
+            '{{%provincias}}',
+            'id',
+            'CASCADE'
+        );
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%gym}}`
+        $this->dropForeignKey(
+            '{{%fk-gym-id_provincia}}',
+            '{{%gym}}'
+        );
+
+        // drops index for column `user_id`
+        $this->dropIndex(
+            '{{%idx-gym-id_provincia}}',
+            '{{%user_gym}}'
+        );
+
+
+        $this->dropColumn('{{%gym}}', 'id_provincia');
+    }
+}
