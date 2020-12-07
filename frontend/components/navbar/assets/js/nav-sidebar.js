35ad1741b29d574fb796646dde9eb5c6902be376
Hector E. Garcia Nuñez <hectoreduardo.garcia@iesdonana.org>
2020-12-07 12:41:35 +0100

    Arreglo algunos fallos con el script

--- frontend/components/navbar/assets/js/nav-sidebar.js
+++ frontend/components/navbar/assets/js/nav-sidebar.js
@@ -1,6 +1,6 @@
 $("#togglebutton").click(function(e) {
     e.preventDefault();
-    $("#sidebar").toggleClass("toggled");
+    $("#wrapper").toggleClass("toggled");
     var $el = $(this).find('svg');
     $el.toggleClass("fa-chevron-left fa-chevron-right");
 });
