a10214b64e214469c42c528555474af8794a395d
Hector E. Garcia Nuñez <hectoreduardo.garcia@iesdonana.org>
2020-12-19 00:47:22 +0100

    prueba

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -5,7 +5,7 @@ use Yii;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
-use common\models\LoginForm;
+use frontend\models\LoginForm;
 
 /**
  * Site controller
