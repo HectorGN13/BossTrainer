5d6ef7ae96186461497947a567945d1e82309ae2
Hector E. Garcia Nuñez <hectoreduardo.garcia@iesdonana.org>
2020-12-22 21:03:26 +0100

    assets de material-desing

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -22,5 +22,6 @@ class AppAsset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap4\BootstrapAsset',
+        'yii\bootstrap4\BootstrapPluginAsset'
     ];
 }
--- frontend/assets/MaterialAsset.php
+++ frontend/assets/MaterialAsset.php
@@ -13,19 +13,23 @@ class MaterialAsset extends AssetBundle
 
     public $css = [
         'css/material-kit.css?v=2.0.4',
+        'css/bootstrap-datepicker.css',
         'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
         'http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons',
     ];
 
     public $js = [
-        "js/plugins/moment.min.js",
+        
         "js/core/jquery.min.js",
         "js/core/popper.min.js",
-        "js/core/bootstrap-material-design.min.js",
+        "js/core/bootstrap-material-design.min.js?v=1.0.0",
+        "js/plugins/moment.min.js",
+        //"js/plugins/bootstrap.js",
+        //"js/plugins/bootstrap-datepicker.js",
         "js/plugins/bootstrap-datetimepicker.js",
         "js/plugins/nouislider.min.js",
         "js/plugins/jquery.sharrre.js",
-        "js/material-kit.js?v=2.0.4"
+        "js/material-kit.js?v=2.0.7"
     ];
 
     public $depends = [
