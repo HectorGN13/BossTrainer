a65eb17747c9ad4e1209624a2397c71da0dfff53
Hector E. Garcia Nuñez <hectoreduardo.garcia@iesdonana.org>
2021-01-05 01:28:11 +0100

    Utilizando ES6
    Función arrow y variables let. close #20

--- backend/web/js/main.js
+++ backend/web/js/main.js
@@ -2,7 +2,7 @@
 document.getElementById("button-up").addEventListener("click", scrollUp);
 
 function scrollUp() {
-	var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+	let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
 	if (currentScroll > 0){
 		window.requestAnimationFrame(scrollUp);
 		window.scrollTo(0,currentScroll - (currentScroll / 10));
@@ -12,7 +12,7 @@ function scrollUp() {
 
  var buttonUp = document.getElementById("button-up");
 window.onscroll = function (){
-	var scroll = document.documentElement.scrollTop;
+	let scroll = document.documentElement.scrollTop;
 	if (scroll > 500) {
 		buttonUp.style.transform = "scale(1)";
 	} else if(scroll < 500 ) {
--- backend/web/js/script.js
+++ backend/web/js/script.js
@@ -1,9 +1,9 @@
 $('.navbar-brand img').hover(function(){
-    $(this).attr("src", function(index, attr){
+    $(this).attr("src", (index, attr) => {
         return attr.replace("logoBlanco.png", "logoColoryBlanco.png");
     });
 }, function(){
-    $(this).attr("src", function(index, attr){
+    $(this).attr("src", (index, attr) => {
         return attr.replace("logoColoryBlanco.png", "logoBlanco.png");
     });
 });
